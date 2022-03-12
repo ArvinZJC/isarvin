c60016e5903c59d805d6e985162a88768f38f5d5
ArvinZJC <tomzjc@qq.com>
2022-03-12 20:54:36 +0800

    fix: attempt to fix preload warnings

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,17 @@
 /*
  * @Description: Vue configuration
- * @Version: 1.0.3.20211206
+ * @Version: 1.0.4.20220312
  * @Author: Arvin Zhao
  * @Date: 2021-06-07 23:43:15
  * @Last Editors: Arvin Zhao
- * @LastEditTime: 2021-12-06 22:15:05
+ * @LastEditTime: 2022-03-12 20:52:21
  */
 
 module.exports = {
   configureWebpack: {
     devtool: "source-map",
   },
+  crossorigin: "anonymous",
   pluginOptions: {
     i18n: {
       locale: "en",
