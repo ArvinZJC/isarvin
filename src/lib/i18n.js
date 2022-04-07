7e7e2c71385e514e050922455e07fc3f2034aca7
ArvinZJC <tomzjc@qq.com>
2022-04-07 21:21:56 +0800

    fix: function referenced before defined

--- src/lib/i18n.js
+++ src/lib/i18n.js
@@ -4,7 +4,7 @@
  * @Author: Arvin Zhao
  * @Date: 2021-08-29 01:12:50
  * @Last Editors: Arvin Zhao
- * @LastEditTime: 2022-04-07 21:19:20
+ * @LastEditTime: 2022-04-07 21:21:15
  */
 
 import { createI18n } from "vue-i18n";
@@ -12,16 +12,6 @@ import { createI18n } from "vue-i18n";
 import global from "./global.js";
 import * as en from "../locales/en.json";
 import * as zhCN from "../locales/zh-CN.json";
-
-const messages = {};
-
-messages[global.common.EN_ID] = en.default;
-messages[global.common.ZH_CN_ID] = zhCN.default;
-
-const i18n = createI18n({
-  locale: decideLanguage(),
-  messages,
-});
 
 /**
  * Decide the site language according to the corresponding user/browser preference.
@@ -44,5 +34,15 @@ export function decideLanguage() {
   document.documentElement.lang = language;
   return language;
 } // end function decideLanguage
+
+const messages = {};
+
+messages[global.common.EN_ID] = en.default;
+messages[global.common.ZH_CN_ID] = zhCN.default;
+
+const i18n = createI18n({
+  locale: decideLanguage(),
+  messages,
+});
 
 export default i18n;
